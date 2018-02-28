061a282e9bf663da0d382345f9f4b892341d2dd5
root <root@host.3golden>
2018-03-01 00:35:33 +0800

    solve the chinese language support problem

--- flaskbb/utils/translations.py
+++ flaskbb/utils/translations.py
@@ -44,6 +44,9 @@ class FlaskBBDomain(Domain):
             return babel.support.NullTranslations()
 
         locale = get_locale()
+	f = open("/data/fla.log","a")
+	print >>f, "locale     %s" %locale
+	f.close()
         cache = self.get_translations_cache()
         translations = cache.get(str(locale))
 
